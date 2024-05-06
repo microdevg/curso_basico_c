184606e3a58f36ec56c961a7dae6a9d62c24a63d
German Velardez <german.velardez@bue.edu.ar>
2024-05-06 20:39:05 +0100

    [Ejemplo 3] Vemos el tamaño de las tipos de datos. Aprendemos sobre printf.

--- ejemplo3/main.c
+++ ejemplo3/main.c
@@ -20,13 +20,15 @@ char nombre[]="German";         // Una secuencia de caracteres, se definen entre
 // Tipos de datos bolean
 
                            // si N diferente de 0 es true. Si N es 0 vale false.
-#define SIZE_IN_BYTES   "tamaño en bytes igual a "
+#define SIZE_IN_BYTES   "tiene un tamaño en bytes igual a "
 int main(void){
     printf("Quiero saber cuantos bytes ocupan los tipos de datos\n");
-    printf("numero_int  %s  %d \n", SIZE_IN_BYTES,sizeof(int));    // %d para numeros enteros con signo
-    printf("numero_int2 %s  %d\n", SIZE_IN_BYTES,sizeof(unsigned int));    // %u para numero sin signo (unsigned)
-    printf("numero_float %s %d\n", SIZE_IN_BYTES,sizeof(float)); // %.2f para numeros con decimales. El 2 es por el numero de decimales utilizado.
-    printf("inicial %s %d\n", SIZE_IN_BYTES,sizeof(char));
-    printf("nombre %s %d \n", SIZE_IN_BYTES,strlen(nombre));
+    printf("int  %s  %d \n", SIZE_IN_BYTES,sizeof(int));    // %d para numeros enteros con signo
+    printf("unsigned int %s  %d\n", SIZE_IN_BYTES,sizeof(unsigned int));    // %u para numero sin signo (unsigned)
+    printf("long %s %d\n",SIZE_IN_BYTES,sizeof(double));
+    printf("float %s %d\n", SIZE_IN_BYTES,sizeof(float)); // %.2f para numeros con decimales. El 2 es por el numero de decimales utilizado.
+    printf("double %s %d \n",SIZE_IN_BYTES,sizeof(double));
+    printf("char %s %d\n", SIZE_IN_BYTES,sizeof(char));
+    printf("nombre= %s %s %d \n",nombre, SIZE_IN_BYTES,strlen(nombre));
     return 0;
 }
