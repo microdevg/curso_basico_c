e0a85b01ac3242c37b8138877ec6b1aefc66821b
German Velardez <german.velardez@bue.edu.ar>
2024-05-06 22:43:33 +0100

    [Ejemplo 1] Directivas.

---
+++ ejemplo1/main.c
@@ -0,0 +1,28 @@
+
+// Esto es un comentario de una linea
+/*
+Esto es un comentario 
+    de 
+    varias lineas
+*/
+
+// Directiva para incluir librería externa
+#include <stdio.h>  
+// Directiva para crear una símbolos con una definición conveniente
+#define NAME    ("German Velardez")
+
+
+
+
+// Definicion de la funcion principal del programa
+
+int main(void){
+    printf("Mi primer programa en C.\n");
+    printf("Este programa fue escrito por: ");
+    printf(NAME);
+    return 0;
+}
+
+// Importante a mencionar:
+// Directivas comienzan con # Y no llevan ; al final
+// Instrucciones deben terminar con punto y coma (;).
